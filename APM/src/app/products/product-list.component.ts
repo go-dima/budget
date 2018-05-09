4ade0fa13ec2fc4a643acd0b24196833cd8fae74
Dima Gonikman <dima.gonikman@gmail.com>
2018-05-09 17:47:52 +0300

    Create a service for products (p9 Services and Dependency Injection)
    - Created ProductService and registered it in the providera of the app (app.component.ts)
    - Injected the service in the c'tor of product-list.component.ts
    - The initializtion happens in the ngOnInit() method, since the service could be a long action and we don't want it to happen on the c'tor.

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -12,6 +13,7 @@ export class ProductListComponent
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    products: IProduct[];
     filteredProducts: IProduct[];
     
     _listFilter: string;
@@ -23,32 +25,7 @@ export class ProductListComponent
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-    ];
-
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private _productService: ProductService) {
     }
 
     toggleImage(): void {
@@ -57,6 +34,8 @@ export class ProductListComponent
 
     ngOnInit(): void {
         console.log("I'm in OnInit!");
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;        
     }
 
     performFilter(filterBy: string): IProduct[] {
