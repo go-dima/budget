1bc4a9f63270b810dffd6189ff45c5d58014902b
Dima Gonikman <dima.gonikman@gmail.com>
2018-05-13 12:21:48 +0300

    Created shared.module (p13 Angular module)
    - Used CLI command
    - Moved Star Componennt and Forms, Common moudles into Shared

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductGuardService } from './product-guard.service';
 import { ProductService } from './product.service';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id',
@@ -20,13 +16,13 @@ import { ProductService } from './product.service';
         component: ProductDetailComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' },      
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent,
   ],
   providers: [
     ProductService,
