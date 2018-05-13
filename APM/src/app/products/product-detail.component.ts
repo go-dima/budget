b75fe9fa090ffe307d24b754397fafc7ff19b3bf
Dima Gonikman <dima.gonikman@gmail.com>
2018-05-13 09:05:32 +0300

    Added route to a product with link (p12 Navigation and Routing Additional Techniques)
    - Also chnaged the logo

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'pm-product-detail',
@@ -11,9 +12,21 @@ export class ProductDetailComponent
     pageTitle: string = "Product Detail";
     product: IProduct;
 
-    constructor() { }
+    constructor(private _route: ActivatedRoute) {  }
 
     ngOnInit() {
+      let id = +this._route.snapshot.paramMap.get('id');
+      this.pageTitle += `: ${id}`;
+      this.product = {
+        "productId": id,
+        "productName": "Dummy",
+        "productCode": "GDN-1337",
+        "releaseDate": "February 31, 2978",
+        "description": "Someday",
+        "price": 10.01,
+        "starRating": 4.9,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+      }
     }
 
 }
