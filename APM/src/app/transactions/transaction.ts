bc2fc31cf9f1c2d3dcc37e9d550e10ac7933affe
Dima Gonikman <dima.gonikman@cyberark.com>
2018-08-22 14:16:27 +0300

    Added ITransaction Interface and TransactionsService

---
+++ APM/src/app/transactions/transaction.ts
@@ -0,0 +1,7 @@
+export interface ITransaction {
+    account: string;
+    date: string;
+    name: string;
+    reference: number;
+    amount: number;
+}
---
+++ APM/src/app/transactions/transactions.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { ITransaction } from "./transaction";
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class TransactiosService {
+    private _transactionsUrl = './api/transactions/transactions.json';
+
+    constructor(private _http: HttpClient) {}
+
+    getHttpTransactions(): Observable<ITransaction[]>{
+        return this._http.get<ITransaction[]>(this._transactionsUrl)
+                   .do(data => console.log('All: ' + JSON.stringify(data)))
+                   .catch(this.handleError);
+    }
+
+    getTransaction(reference: number): Observable<ITransaction> {
+        return this.getHttpTransactions()
+            .map((transactions: ITransaction[]) => transactions.find(p => p.reference === reference));
+    }
+
+    handleError(err: HttpErrorResponse) {
+        console.log(err.message);
+        return Observable.throw(err.message);
+    }
+}
