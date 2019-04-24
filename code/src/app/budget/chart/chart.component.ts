cb7a402eea647079ad9f791903ef1fcbe9ad8f29
Dima Gonikman <dima.gonikman@gmail.com>
2019-04-24 22:24:37 +0300

    Refactor chart.component

--- code/src/app/budget/chart/chart.component.ts
+++ code/src/app/budget/chart/chart.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
 import { BalanceReportService } from '../balance-report/balance-report.service';
 import { IReportEntry } from '../IReportEntry';
-import * as _ from 'lodash';
+import { map } from 'lodash';
 
 @Component({
   selector: 'pm-chart',
@@ -11,20 +11,20 @@ import * as _ from 'lodash';
 })
 export class ChartComponent implements OnInit {
   @ViewChild('lineChart') private chartRef;
-  chart: any; 
+  chart: any;
 
-  constructor(private _balanceReportService: BalanceReportService) {}
+  constructor(private _balanceReportService: BalanceReportService) { }
 
   ngOnInit() {
     this._balanceReportService.generatedReport.subscribe(($event: IReportEntry[]) => this.onReportGenerated($event));
   }
 
   onReportGenerated(report: IReportEntry[]) {
-    let labels = _.map(report, entry => entry.key);
-    let balanceData = _.map(report, entry => entry.balance);
-    let incomeData = _.map(report, entry => entry.income.total);
-    let expenseData = _.map(report, entry => entry.expense.total);
-    
+    let labels = map(report, entry => entry.key);
+    let balanceData = map(report, entry => entry.balance);
+    let incomeData = map(report, entry => entry.income.total);
+    let expenseData = map(report, entry => entry.expense.total);
+
     // clear old data, if exists
     if (this.chart != undefined)
       this.chart.destroy();
@@ -34,15 +34,21 @@ export class ChartComponent implements OnInit {
       data: {
         labels: labels,
         datasets: [
-          { data: balanceData, backgroundColor: 'rgba(0, 0, 255, 0.3)' },
-          { data: incomeData, backgroundColor: 'rgba(0, 255, 0, 0.3)' },
-          { data: expenseData, backgroundColor: 'rgba(255, 0, 0, 0.3)' }
+          { 
+            label: 'Balance', data: balanceData, backgroundColor: 'rgba(0, 0, 255, 0.3)'
+          },
+          {
+            label: 'Income', data: incomeData, backgroundColor: 'rgba(0, 255, 0, 0.3)'
+          },
+          {
+            label: 'Expense', data: expenseData, backgroundColor: 'rgba(255, 0, 0, 0.3)'
+          }
         ]
       },
       options: {
         responsive: false,
         legend: {
-          display: false
+          display: true
         },
         scales: {
           xAxes: [{
