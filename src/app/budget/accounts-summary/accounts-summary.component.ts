0ffcd1c01c045854cfe001e0fe8687ce53390a7b
Dima Gonikman <dima.gonikman@gmail.com>
2020-06-02 23:39:40 +0300

    Filter summary by categories

--- src/app/budget/accounts-summary/accounts-summary.component.ts
+++ src/app/budget/accounts-summary/accounts-summary.component.ts
@@ -15,68 +15,95 @@ export class AccountsSummaryComponent implements OnInit {
   transactions: ITransaction[];
   accounts: IAccountSummary[];
   byAccount: { [key: string] : ITransaction[]} = {};
+  orderedTransactions: ITransaction[];
+  selectedCategories: string[];
+  yearlyTransactions: ITransaction[];
 
   constructor(private _transactionsService: TransactiosService) { }
 
   ngOnInit(): void {
     this._transactionsService.getAllTransactions().subscribe(
       transactions => {
-          this.clear();
-          let orderedTransactions = orderBy(transactions, Common.transactionDateKey, 'desc');
-          this.accounts = uniqBy(orderedTransactions, 'account').map(this.accountSummary);
-          this.accounts.forEach(account => {
-            this.byAccount[account.name] = orderedTransactions.filter((t: ITransaction) => t.account == account.name);
-            let latestTransaction = this.byAccount[account.name][0]
-            
-            const incomeFilter = (t: ITransaction) => t.amount > 0;
-            const expenseFilter = (t: ITransaction) => t.amount < 0;
+        this.clear();
+        this.orderedTransactions = orderBy(transactions, Common.transactionDateKey, 'desc');
+        const yearAgo: string = yearBack(this.orderedTransactions[0].date);
+        this.yearlyTransactions = this.orderedTransactions.filter((t: ITransaction) => DateUtils.isBigger(t.date, yearAgo))
 
-            account.balance = latestTransaction.balance
-            account.date = latestTransaction.date
-            account.income_average = this.calcAverage(this.byAccount[account.name], incomeFilter);
-            account.expense_average = this.calcAverage(this.byAccount[account.name], expenseFilter);
-            account.net_average = account.income_average + account.expense_average;
-          });
-          
-          let totals: IAccountSummary = {
-            name: "Total",
-            balance:  Common.accumulate(this.accounts.map(a => a.balance)),
-            date: this.accounts.map(a => a.date).reduce((a, b) => DateUtils.max(a, b)),
-            income_average: Common.accumulate(this.accounts.map(a => a.income_average)),
-            expense_average: Common.accumulate(this.accounts.map(a => a.expense_average)),
-            net_average: Common.accumulate(this.accounts.map(a => a.net_average))
-          };
-          this.accounts.unshift(totals);
+        this.accounts = uniqBy(this.orderedTransactions, 'account').map(accountSummary);
+        let totals: IAccountSummary = newAccountSummary("Total");
+        this.accounts.unshift(totals);
+        this.reload();
       });
+  }
+  
+  private reload() {
+    this.accounts.forEach(account => {
+      let relevantTransactions = this.selectedCategories ?
+        this.yearlyTransactions.filter((t: ITransaction) => this.selectedCategories.includes(t.category)) :
+        this.yearlyTransactions
+      const latestTransaction = this.orderedTransactions.filter(t => t.account == account.name)[0]
+      account.balance = latestTransaction ? latestTransaction.balance : 0;
+      
+      this.byAccount[account.name] = relevantTransactions.filter((t: ITransaction) => t.account == account.name);
+      const latestYearlyTransaction = this.byAccount[account.name][0];
+      account.date = latestYearlyTransaction? latestYearlyTransaction.date : "01/01/1900";
+
+      const incomeFilter = (t: ITransaction) => t.amount > 0;
+      const expenseFilter = (t: ITransaction) => t.amount < 0;
+      account.balance = latestTransaction ? latestTransaction.balance : 0;
+      account.date = latestTransaction? latestTransaction.date : "01/01/1900";
+      account.income_average = this.calcAverage(this.byAccount[account.name], incomeFilter);
+      account.expense_average = this.calcAverage(this.byAccount[account.name], expenseFilter);
+      account.net_average = account.income_average + account.expense_average;
+    });
+    this.accounts[0].balance = Common.accumulate(this.accounts.map(a => a.balance))
+    this.accounts[0].date = this.accounts.map(a => a.date).reduce((a, b) => DateUtils.max(a, b))
+    this.accounts[0].income_average = Common.accumulate(this.accounts.map(a => a.income_average))
+    this.accounts[0].expense_average = Common.accumulate(this.accounts.map(a => a.expense_average))
+    this.accounts[0].net_average = Common.accumulate(this.accounts.map(a => a.net_average))
   }
 
   private calcAverage(sourceTransactions: ITransaction[], customFilter: (t: ITransaction) => boolean): number {
-    const yearBack: string = this.yearBack(sourceTransactions[0].date);
-    const latestTranasactions: ITransaction[] = sourceTransactions.filter((t: ITransaction) => DateUtils.isBigger(t.date, yearBack))
+    if (!sourceTransactions || sourceTransactions.length == 0)
+    return 0
+    const yearAgo: string = yearBack(sourceTransactions[0].date);
+    const latestTranasactions: ITransaction[] = sourceTransactions.filter((t: ITransaction) => DateUtils.isBigger(t.date, yearAgo))
     const filteredAmounts: number[] = latestTranasactions.filter(customFilter).map((t: ITransaction) => t.amount);
     const filteredSum = filteredAmounts.length > 0 ? Common.accumulate(filteredAmounts) : 0;
     const filteredAvg = filteredSum / 12 || 0;
     return filteredAvg;
   }
-
-  accountSummary(transaction: ITransaction): IAccountSummary {
-    return {
-      name: transaction.account,
-      balance: 0,
-      income_average : 0,
-      expense_average : 0,
-      net_average: 0,
-      date: "01/01/1900"
-    }
-  }
-
+  
   clear() {
     this.accounts = []
   }
-
-  yearBack(date: string) : string {
-    let year = Number(date.slice(-4))
-    let yearBack = date.slice(0, -4) + (year - 1).toString()
-    return yearBack;
+  
+  selectedCategoriesChanged(selectedCategories: string[]) {
+    this.selectedCategories = selectedCategories;
+    this.reload()
   }
 }
+
+function newAccountSummary(newName: string): IAccountSummary {
+  return {
+    name: newName,
+    balance: 0,
+    date: "01/01/1900",
+    income_average: 0,
+    expense_average: 0,
+    net_average: 0
+  };
+}
+
+function accountSummary(transaction: ITransaction): IAccountSummary {
+  return newAccountSummary(transaction.account)
+}
+
+function yearBack(date: string) : string {
+  let year = Number(date.slice(-4))
+  let yearBack = date.slice(0, -4) + (year - 1).toString()
+  return yearBack;
+}
+
+
+
