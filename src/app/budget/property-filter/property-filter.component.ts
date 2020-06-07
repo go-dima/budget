d746e5ddcacd71ddfbbb53a355e34c5281dd8503
Dima Gonikman <dima.gonikman@gmail.com>
2020-06-07 20:51:18 +0300

    Inline accounts

--- src/app/budget/property-filter/property-filter.component.ts
+++ src/app/budget/property-filter/property-filter.component.ts
@@ -14,6 +14,7 @@ import Common from '../common';
 export class PropertyFilterComponent implements OnInit {
   @Input() filterProperty: string;
   @Input() customProperties: ReplaySubject<ICheckbox[]>;
+  @Input() showInline: boolean = false;
   @Output() selectedProperties: EventEmitter<string[]> = new EventEmitter<string[]>();
   properties: ICheckbox[];
   errorMessage: string;
